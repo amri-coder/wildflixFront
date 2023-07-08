03ba4d0f5d047bca28571a209c39aac84e99cf23
DADOU STEVEN <dgv1pro@gmail.com>
2023-07-08 11:03:23 +0200

    dadou

---
+++ src/environments/environments.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EnvironmentsRoutingModule } from './environments-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EnvironmentsRoutingModule
+  ]
+})
+export class EnvironmentsModule { }
