1f4b8d9261da961f567cbb6bbf053370f13a4e77
DADOU STEVEN <dgv1pro@gmail.com>
2023-07-12 10:07:31 +0200

    dadou1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,72 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {PagesModule} from "./pages/pages.module";
-import { FavoriComponent } from '../services/favori/favori.component';
-import { SocialComponent } from '../services/social/social.component';
-import { UploadDownloadComponent } from '../services/upload-download/upload-download.component';
-import {ServicesModule} from "../services/services.module";
-import {OtherPagesModule} from "./other-pages/other-pages.module";
+import { PreloaderComponent } from './components/common/preloader/preloader.component';
+import { HomeOneComponent } from './components/pages/home-one/home-one.component';
+import { HomeTwoComponent } from './components/pages/home-two/home-two.component';
+import { HomeThreeComponent } from './components/pages/home-three/home-three.component';
+import { NavbarStyleOneComponent } from './components/common/navbar-style-one/navbar-style-one.component';
+import { NavbarStyleTwoComponent } from './components/common/navbar-style-two/navbar-style-two.component';
+import { FooterStyleOneComponent } from './components/common/footer-style-one/footer-style-one.component';
+import { FooterStyleTwoComponent } from './components/common/footer-style-two/footer-style-two.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { TeamComponent } from './components/pages/team/team.component';
+import { PricingComponent } from './components/pages/pricing/pricing.component';
+import { CategoryComponent } from './components/pages/category/category.component';
+import { FaqComponent } from './components/pages/faq/faq.component';
+import { TestimonialsComponent } from './components/pages/testimonials/testimonials.component';
+import { ErrorComponent } from './components/pages/error/error.component';
+import { LoginRegisterComponent } from './components/pages/login-register/login-register.component';
+import { TermsConditionComponent } from './components/pages/terms-condition/terms-condition.component';
+import { PrivacyPolicyComponent } from './components/pages/privacy-policy/privacy-policy.component';
+import { ComingSoonComponent } from './components/pages/coming-soon/coming-soon.component';
+import { ShopComponent } from './components/pages/shop/shop.component';
+import { CartComponent } from './components/pages/cart/cart.component';
+import { CheckoutComponent } from './components/pages/checkout/checkout.component';
+import { ProductsDetailsComponent } from './components/pages/products-details/products-details.component';
+import { ListingComponent } from './components/pages/listing/listing.component';
+import { ListingDetailsComponent } from './components/pages/listing-details/listing-details.component';
+import { BlogComponent } from './components/pages/blog/blog.component';
+import { BlogDetailsComponent } from './components/pages/blog-details/blog-details.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FavoriComponent,
-    SocialComponent,
-    UploadDownloadComponent
+    PreloaderComponent,
+    HomeOneComponent,
+    HomeTwoComponent,
+    HomeThreeComponent,
+    NavbarStyleOneComponent,
+    NavbarStyleTwoComponent,
+    FooterStyleOneComponent,
+    FooterStyleTwoComponent,
+    AboutComponent,
+    TeamComponent,
+    PricingComponent,
+    CategoryComponent,
+    FaqComponent,
+    TestimonialsComponent,
+    ErrorComponent,
+    LoginRegisterComponent,
+    TermsConditionComponent,
+    PrivacyPolicyComponent,
+    ComingSoonComponent,
+    ShopComponent,
+    CartComponent,
+    CheckoutComponent,
+    ProductsDetailsComponent,
+    ListingComponent,
+    ListingDetailsComponent,
+    BlogComponent,
+    BlogDetailsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    PagesModule,
-    ServicesModule,
-    OtherPagesModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
