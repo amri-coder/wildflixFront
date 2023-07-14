d3bd7ec568cb10b2d1f52aa25c4f63ed1b082767
Maher <amri.site@yahoo.com>
2023-07-14 22:01:28 +0200

    Modification in the contact form for add controls

--- src/app/components/pages/contact/contact.component.ts
+++ src/app/components/pages/contact/contact.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+    selector: "app-contact",
+    templateUrl: "./contact.component.html",
+    styleUrls: ["./contact.component.scss"],
 })
 export class ContactComponent implements OnInit {
+    submitted: boolean = false;
 
-  constructor() { }
+    constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
+    contactForm: FormGroup = this.fb.group({
+        name: ["", [Validators.required, Validators.min(3)]],
+        email: ["", [Validators.required, Validators.email, Validators.min(4)]],
+        message: ["", [Validators.required, Validators.min(4)]],
+    });
+    get f() {
+        return this.contactForm.controls;
+    }
 
+    ngOnInit(): void {}
+
+    onSubmit() {
+        this.submitted = true;
+    }
 }
