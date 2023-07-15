d351c33958960274bb6882b7c7f73a50b0eb2ee1
Maher <amri.site@yahoo.com>
2023-07-15 09:50:49 +0200

    Modification in contact component and add service method

--- src/app/components/pages/contact/contact.component.ts
+++ src/app/components/pages/contact/contact.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from "src/app/services/auth.service";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Component, OnInit } from "@angular/core";
 
@@ -7,13 +8,24 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./contact.component.scss"],
 })
 export class ContactComponent implements OnInit {
+    confirmMsg: string = "";
+    errorMsg: string = "";
+
     submitted: boolean = false;
 
-    constructor(private fb: FormBuilder) {}
+    name: string = "";
+    email: string = "";
+    phone: string = "";
+    subject: string = "";
+    message: string = "";
+
+    constructor(private fb: FormBuilder, private authService: AuthService) {}
 
     contactForm: FormGroup = this.fb.group({
         name: ["", [Validators.required, Validators.min(3)]],
         email: ["", [Validators.required, Validators.email, Validators.min(4)]],
+        phone: [""],
+        subject: [""],
         message: ["", [Validators.required, Validators.min(4)]],
     });
     get f() {
@@ -24,5 +36,15 @@ export class ContactComponent implements OnInit {
 
     onSubmit() {
         this.submitted = true;
+        this.confirmMsg = "";
+        this.errorMsg = "";
+        this.authService.sendContactForm(this.contactForm.value).subscribe(
+            (response) => {
+                this.confirmMsg = response.message;
+            },
+            (error) => {
+                this.errorMsg = error.error.message;
+            }
+        );
     }
 }
